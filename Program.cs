1eeb3904548a9a19486e2fb6f44e63e71cb20537
Ikhsan Abdul Rafi <107069762+icanselalubenar@users.noreply.github.com>
2023-04-04 20:04:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Input nilai a : ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Input nilai b : ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Input nilai c : ");
+            c = int.Parse(Console.ReadLine());
+
+            classNum Nilai = new classNum();
+            Nilai.cariNilaiMin(a, b, c);
+            Nilai.cariNilaiMax(a, b, c);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ classNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Overload
+{
+    public class classNum
+    {
+        public int cariNilaiMin(int a, int b, int c)
+        {
+            int min = 0;
+            if (a < b && a < c)
+                Console.WriteLine("Nilai minimum dari ketiga nilai adalah = {0}", min = a);
+            else if (b < a && b < c)
+                Console.WriteLine("Nilai minimum dari ketiga nilai adalah = {0}", min = b);
+            else if (c < a && c < b)
+                Console.WriteLine("Nilai minimum dari ketiga nilai adalah = {0}", min = c);
+
+            return min;
+        }
+        public int cariNilaiMax(int a, int b, int c)
+        {
+            int max = 0;
+            if (a > b && a > c)
+                Console.WriteLine("Nilai maksimum dari ketiga nilai adalah = {0}", max = a);
+            else if (b > a && b > c)
+                Console.WriteLine("Nilai maksimum dari ketiga nilai adalah = {0}", max = b);
+            else if (c > a && c > b)
+                Console.WriteLine("Nilai maksimum dari ketiga nilai adalah = {0}", max = c);
+
+            return max;
+        }
+    }
+}
+
